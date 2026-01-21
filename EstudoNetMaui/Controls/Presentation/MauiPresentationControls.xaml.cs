742309567cc88d089db83be60ae241caefdc0ee7
Willian <williandasilvaalmeida3@gmail.com>
2026-01-21 00:17:12 -0300

    Estudo sobre os controles de apresentação do maui

---
+++ EstudoNetMaui/Controls/Presentation/MauiPresentationControls.xaml.cs
@@ -0,0 +1,9 @@
+namespace EstudoNetMaui.Controls.Presentation;
+
+public partial class MauiPresentationControls : ContentPage
+{
+	public MauiPresentationControls()
+	{
+		InitializeComponent();
+	}
+}
